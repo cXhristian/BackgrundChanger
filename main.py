b14caef625d090b3b57f19670783eb86f48bcd79
Christian Duvholt <cxhristian@gmail.com>
2013-09-23 12:33:42 +0200

    Cleaned up and made code more modular

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 from subprocess import Popen, PIPE
 import requests
 from pyquery import PyQuery
 import re
 from random import choice
 from os.path import abspath
+
+DEBUG = True
 
 
 class ScrapeException(Exception):
@@ -14,36 +17,37 @@ class Scrape:
 	filters = {}
 
 	def __init__(self):
-		self.query = 'nature'
+		self.background = Background()
+		self.filters = {}
+		self.base_url = None
 
 	def url(self):
-		raise NotImplemented
+		if self.filters:
+			return self.base_url + self.filters_querystring()
+		else:
+			raise ScrapeException('No fiters set')
 
 	def load(self):
-		if self.url and self.query:
-			r = requests.get(self.url())
-			if r.status_code == 200:
-				return r.text
+		if DEBUG:
+			print(self.url())
+		r = requests.get(self.url())
+		if r.status_code == 200:
+			return r.text
 		raise ScrapeException('Failed to download')
 
 	def filters_querystring(self):
 		"""Turns a dict into a querystring"""
-		return '&'.join([str(key) + '=' + str(value) for key, value in self.filters.items()])
+		return '?' + '&'.join([str(key) + '=' + str(value) for key, value in self.filters.items()])
 
 
 class Wallbase(Scrape):
 
 	def __init__(self):
 		super().__init__()
-
-	def url(self):
-		if self.query:
-			return 'http://wallbase.cc/search?q=' + self.query + self.filters_querystring()
-		else:
-			raise ScrapeException
+		self.base_url = 'http://wallbase.cc/search'
 
 	def parse(self):
-		"""Parses html for thumbnails and returns full image url"""
+		"""Parses html for thumbnails and returns full image urls"""
 		pq = PyQuery(self.load())
 		thumbs = pq('section#thumbs > .thumbnail img')
 		return 	[self.thumb2full(thumb) for thumb in thumbs]
@@ -52,6 +56,17 @@ class Wallbase(Scrape):
 		thumb_name = pq.attrib['data-original']
 		groups = lambda n: re.match('http://thumbs\.wallbase\.cc//?(.+)/thumb-(\d+)\.jpg', thumb_name).group(n)
 		return 'http://wallpapers.wallbase.cc/' + groups(1) + '/wallpaper-' + groups(2) + '.jpg'
+
+	def random_search(self, query):
+		"""Searches with random ordering"""
+		if query:
+			self.filters = {'order': 'random', 'thpp': 1, 'q': query}
+		self.background.set_background(self.parse())
+
+	def search(self, query):
+		"""Searches with default sorting and takes a random background from the first 20 results"""
+		self.filters['q'] = query
+		self.background.set_background(self.parse())
 
 
 class Background:
@@ -66,23 +81,21 @@ class Background:
 	def set(self, image):
 		return self.popen('gsettings set org.gnome.desktop.background picture-uri ' + image)
 
-	def random(self, service):
-		# http://wallbase.cc/search?q=nature&order=random&thpp=1
-		service.filter = {'order': 'random', 'thpp': 1}
-
-		raise NotImplemented
+	def set_background(self, backgrounds):
+		"""Takes several backgrounds, selects a random and saves it"""
+		background = choice(backgrounds)
+		self.save(background)
 
 	def save(self, background):
-		with open('temp.jpg', 'bw+') as temp:
+		"""Saves background and sets it as wallpaper"""
+		filename = abspath('temp.jpg')
+		with open(filename, 'bw+') as temp:
 			temp.write(requests.get(background).content)
+		self.set('file://' + filename)
+		if DEBUG:
+			print(self.get())
+		return filename
 
-
-b = Background()
 w = Wallbase()
-w.query = 'nature'
-backgrounds = w.parse()
-background = choice(backgrounds)
-b.save(background)
-print(background)
-b.set('file://' + abspath('temp.jpg'))
-print(b.get())
+#w.search('robot')
+w.random_search('nature')
